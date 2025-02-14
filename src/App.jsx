188cf24504b75def028560d6d9d20e0089638ee8
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-02-14 10:44:33 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import MovesCounter from './components/MovesCounter';
 import { useState } from 'react';
 import './App.css';
-/* TODO:
-- User story 1:
-- Create a counter
-  - display a number to track moves
-  - starts at 0 with the option to increase later
 
-- Create a card
-  - back of card 
-    - same image
-  - front of card
-    - different images
-*/ 
 
 
 function Card() {
